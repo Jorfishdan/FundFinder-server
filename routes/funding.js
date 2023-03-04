d09bb17fdd426e7d094aefd713d763f4f66a30d5
Jorfishdan <jordan_fish@ymail.com>
2023-03-04 10:29:21 -0800

    added routes and json file for funding options

---
+++ routes/funding.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const { type } = require("os");
+const funding = require("../data/funding.json");
+
+router.get("/", (req, res) => {
+    const fundingGallery = fundingList();
+    const displayFunding = fundingGallery.map((info) => {
+      return {
+        id: info.id,
+        name: info.name,
+      type : info.type,
+      location: info.location,
+      gender: info.location,
+      posted : info.posted
+      };
+    });
+    res.send(displayFunding);
+  });
+
+  function fundingList() {
+    const fundingPreview = fs.readFileSync("./data/funding.json");
+    return JSON.parse(fundingPreview);
+  }
+
+module.exports = router;
