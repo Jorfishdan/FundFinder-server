656bf6254879c956b083cb513ba0348186c30427
Jorfishdan <jordan_fish@ymail.com>
2023-03-05 11:34:06 -0800

    updated funding json and funding route

--- routes/funding.js
+++ routes/funding.js
@@ -5,15 +5,18 @@ const { type } = require("os");
 const funding = require("../data/funding.json");
 
 router.get("/", (req, res) => {
+  const { location, gender } = req.query;
     const fundingGallery = fundingList();
-    const displayFunding = fundingGallery.map((info) => {
+    const displayFunding = fundingGallery.filter((info) => {
+      return info.location.includes(location) && info.gender.includes(gender);
+  }).map((info) => {
       return {
         id: info.id,
         name: info.name,
-      type : info.type,
-      location: info.location,
-      gender: info.gender,
-      posted : info.posted
+        type : info.type,
+        location: info.location,
+        gender: info.gender,
+        posted : info.posted
       };
     });
     res.send(displayFunding);
